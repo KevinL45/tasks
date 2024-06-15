0c5a7e55a479c286bc8dc52a5b6faeb289e0dd3c
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2024-06-15 18:08:40 +0200

    affectation

--- front/src/views/AffectationView.vue
+++ front/src/views/AffectationView.vue
@@ -3,4 +3,12 @@
     <div class="affectation">
       <h1>Affectation</h1>
     </div>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Tache</h5>
+    <p class="card-text">Nom de l'employée</p>
+    <p class="card-text">Heure de début et fin</p>
+    <a href="#" class="btn btn-success">Affectation</a>
+  </div>
+</div>
   </template>
