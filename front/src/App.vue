39de543b57cff7aec3fb7288a20ef8b2511ac616
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2024-06-15 14:58:00 +0200

    correction

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <NavbarComponent/>
-  <br>
-  <HomePage/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HomePage from './components/HomePage.vue';
-import NavbarComponent from './layout/NavbarComponent.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HomePage,
-    NavbarComponent
-  } 
-}
-</script>
 
 <style>
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
