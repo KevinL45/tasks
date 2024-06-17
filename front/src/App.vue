6124f1685eb868b89add4d149f214f351c67ebcb
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2024-06-17 02:17:23 +0200

    navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,24 +1,19 @@
 <template>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Liste des tâches</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/createtask">Créer une tâche</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/affecttask">Affecter une tâches</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  </nav>
+  <Navbar />
   <br>
   <router-view/>
 </template>
+<script>
+import Navbar from "@/layout/Navbar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+};
+
+</script>
 
 <style>
 #app {
---
+++ front/src/layout/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Liste des tâches</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/createtask">Créer une tâche</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/affecttask">Affecter une tâches</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </nav>
+    <br>
+</template>
+ <script>
+export default {
+  name: 'NavbarComponent'
+};
+</script>
